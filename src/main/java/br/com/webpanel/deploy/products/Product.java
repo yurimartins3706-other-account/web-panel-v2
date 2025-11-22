fd3306375e0ecea079c5b4f130b1fbd06360e8af
yurimartins3706-other-account <yurimartins3706@gmail.com>
2025-11-22 21:56:39 +0000

    update em produto

--- src/main/java/br/com/webpanel/deploy/products/Product.java
+++ src/main/java/br/com/webpanel/deploy/products/Product.java
@@ -70,7 +70,7 @@ public class Product {
     )
     private Set<Image> images;
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private Set<ProductCustomAttributeValue> customAttributeValues;
 
     private Instant createdAt;
